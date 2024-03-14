02bfe19f8b0c9871cfffd6a7ccc264a867686d12
harshak02 <sreeharshak002@gmail.com>
2024-03-14 16:38:40 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,51 @@
 /*=============== SEARCH ===============*/
+const searchButton = document.getElementById("search-button");
+const searchContent = document.getElementById("search-content");
+const searchClose = document.getElementById("search-close");
 
+if (searchButton) {
+  searchButton.addEventListener("click", () => {
+    searchContent.classList.add("show-search");
+  });
+}
+
+if (searchClose) {
+  searchClose.addEventListener("click", () => {
+    searchContent.classList.remove("show-search");
+  });
+}
 
 /*=============== LOGIN ===============*/
+const loginButton = document.getElementById("login-button");
+const loginContent = document.getElementById("login-content");
+const loginClose = document.getElementById("login-close");
 
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    loginContent.classList.add("show-login");
+  });
+}
+
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    loginContent.classList.remove("show-login");
+  });
+}
 
 /*=============== ADD SHADOW HEADER ===============*/
 
-
 /*=============== HOME SWIPER ===============*/
-
 
 /*=============== FEATURED SWIPER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
-
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
